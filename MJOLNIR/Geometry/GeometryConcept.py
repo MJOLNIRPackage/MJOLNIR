c92811086dee91d30a166a5aa44e5533db183697
Jakob Lass <lass.jakob@gmail.com>
2018-03-09 14:14:47 +0100

    Cleanup

--- MJOLNIR/Geometry/GeometryConcept.py
+++ MJOLNIR/Geometry/GeometryConcept.py
@@ -119,31 +119,6 @@ class GeometryObject(GeometryConcept):
         direction/=np.linalg.norm(direction)
         self._direction = direction
 
-    # def setDirection(self,direction):
-    #     self.mDirection = np.array(direction)
-    
-    # def setDirectionRadians(self,angles):
-    #     xdirection = math.sin(angles[1])*math.cos(angles[0])
-    #     ydirection = math.sin(angles[1])*math.sin(angles[0])
-    #     zdirection = math.cos(angles[1])
-    #     self.mDirection = (xdirection,ydirection,zdirection)
-
-    # def setDirectionDegrees(self,anglesd):
-    #     anglesConverted = (anglesd[1]*math.pi/180.0,anglesd[0]*math.pi/180.0,anglesd[1]*math.pi/180.0)
-    #     self.setDirectionRadians(anglesConverted)
-
-    # def getDirectionRadians(self):
-    #     theta = math.atan2(self.mDirection[1],self.mDirection[0])
-    #     phi = math.acos(self.mDirection[2])
-    #     return [theta,phi]
-
-    # def getDirectionDegrees(self):
-    #     angles = getDirectionRadians()
-    #     return [math.degrees(angles[0]),math.degrees(angles[1])]
-
-
-    def __repr__(self):
-        return "%s(%r)" % (self.__class__, self.__dict__)
 
     def __str__(self):
         return "Position = {}\tDirection = {}".format(self._position,self._direction)
