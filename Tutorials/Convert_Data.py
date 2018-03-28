f5bc666bcb00b07b4ebf61adfa2a2dd9827081d0
Jakob Lass <lass.jakob@gmail.com>
2018-03-28 10:31:31 +0200

    Update Tutorials with relative data paths

--- Tutorials/Convert_Data.py
+++ Tutorials/Convert_Data.py
@@ -4,13 +4,13 @@ sys.path.append('..')
 from MJOLNIR.Geometry import Instrument
 from MJOLNIR.Data import DataSet
 
-Instr = Instrument.Instrument(filename='TestData/CAMEA_Full.xml')
+Instr = Instrument.Instrument(filename='../TestData/CAMEA_Full.xml')
 Instr.initialize()
 
-NF = 'TestData/VanNormalization.h5'
+NF = '../TestData/VanNormalization.h5'
 dataset = DataSet.DataSet(instrument=Instr,normalizationfiles=NF)
 
-normalizationfile = 'TestData/EnergyNormalization_8.calib'
+normalizationfile = '../TestData/EnergyNormalization_8.calib'
 
-DataFiles = 'TestData/VanNormalization.h5'
+DataFiles = '../TestData/VanNormalization.h5'
 dataset.ConvertDatafile(datafiles=DataFiles,normalizationfile=normalizationfile)
--- Tutorials/Generate_normalization.py
+++ Tutorials/Generate_normalization.py
@@ -4,10 +4,10 @@ sys.path.append('..')
 from MJOLNIR.Geometry import Instrument
 from MJOLNIR.Data import DataSet
 
-Instr = Instrument.Instrument(filename='TestData/CAMEA_Full.xml')
+Instr = Instrument.Instrument(filename='../TestData/CAMEA_Full.xml')
 Instr.initialize()
 
-NF = 'TestData/VanNormalization.h5'
+NF = '../TestData/VanNormalization.h5'
 
 dataset = DataSet.DataSet(instrument=Instr,normalizationfiles=NF)
-dataset.EnergyCalibration(NF,'TestData/',plot=True,tables=['PrismaticHighDefinition',3]) 
+dataset.EnergyCalibration(NF,'../TestData/',plot=False,tables=['PrismaticHighDefinition',3]) 
