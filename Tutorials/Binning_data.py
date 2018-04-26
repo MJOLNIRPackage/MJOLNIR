9299e69f1bb0225e6d946ac2b702010354956e34
Jakob Lass <lass.jakob@gmail.com>
2018-04-26 10:27:46 +0200

    Change filename to fileName

--- Tutorials/Binning_data.py
+++ Tutorials/Binning_data.py
@@ -5,9 +5,9 @@ from MJOLNIR.Data import DataSet,Viewer3D
 import numpy as np
 import h5py as hdf
 import matplotlib.pyplot as plt
-filename = '../../CAMEA_Test_Files/cameasim2018n000011.nxs'
+fileName = '../../CAMEA_Test_Files/cameasim2018n000011.nxs'
 
-file = hdf.File(filename,'r')
+file = hdf.File(fileName,'r')
 
 I = np.array(file.get('entry/data/intensity'))
 posx = np.array(file.get('entry/data/qx'))
