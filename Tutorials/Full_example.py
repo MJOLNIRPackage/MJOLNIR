4ee46366b959849bf8d422579a465f9cb7fc1c07
Jakob Lass <lass.jakob@gmail.com>
2018-07-11 09:24:34 +0200

    Change data path to relative

--- Tutorials/Full_example.py
+++ Tutorials/Full_example.py
@@ -5,10 +5,10 @@ from MJOLNIR.Data import DataSet,Viewer3D
 import warnings
 import matplotlib.pyplot as plt
 import numpy as np
-DataFile = '/home/lass/Dropbox/PhD/Software/DataSimulation/BeFilterTestOut10.h5'#['../TestData/cameasim2018n000011.h5']
+DataFile = ['../TestData/cameasim2018n000011.h5']
 
 dataset = DataSet.DataSet(dataFiles=DataFile)
-dataset.convertDataFile(saveLocation='/home/lass/Dropbox/PhD/Software/DataSimulation/')
+dataset.convertDataFile(saveLocation='../TestData/')
 
 
 Data,bins = dataset.binData3D(0.08,0.08,0.25)
