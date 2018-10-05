8f3e6a2c43e89337886557ffd49bfc476279e9a0
Jakob Lass <lass.jakob@gmail.com>
2018-10-05 11:42:40 +0200

    Expand KwargChecker to allow function and list of accepted inputs

--- MJOLNIR/_tools.py
+++ MJOLNIR/_tools.py
@@ -2,52 +2,54 @@ import sys
 #import warnings
 import numpy as np
 from difflib import SequenceMatcher
+import functools
 
-def KwargChecker(func):
-    """Function to check if given key-word is in the list of accepted Kwargs. If not directly therein, checks capitalization. If still not match raises error
-    with suggestion of closest argument.
+def KwargChecker(function=None,include=None):
     
-    Args:
-    
-        - func (function): Function to be decorated.
+    def KwargCheckerNone(func):
+        """Function to check if given key-word is in the list of accepted Kwargs. If not directly therein, checks capitalization. If still not match raises error
+        with suggestion of closest argument.
+        
+        Args:
+        
+            - func (function): Function to be decorated.
 
-    Raises:
+        Raises:
 
-        - AttributeError
-    """
-    def newFunc(self,*args,**kwargs):
-        def checkArgumentList(argList,kwargs):
-            for key in kwargs:
-                #if key.lower() in [x.lower() for x in argList] and key not in argList:
-                #    expected = argList[[x.lower() for x in argList].index(key.lower())]
-                    #warnings.warn(message='Did you mean "{}" in stead of "{}"?'.format(expected,key),category=SyntaxWarning,stacklevel=3)
+            - AttributeError
+        """
+        @functools.wraps(func)
+        def newFunc(self,*args,**kwargs):
+            N = func.__code__.co_argcount            
+            argList = list(newFunc._original.__code__.co_varnames[:N])
+            if not function is None:
+                for arg in function.__code__.co_varnames[:function.__code__.co_argcount]:
+                    argList.append(str(arg))
+            if not include is None:
+                for arg in include:
+                    argList.append(str(arg))
+            checkArgumentList(argList,kwargs)
+            returnval = func(self,*args,**kwargs)
+            return returnval
+        newFunc._original = func
+        return newFunc
+    return KwargCheckerNone
 
-                if key not in argList:
-                    similarity = np.array([SequenceMatcher(None, key.lower(), x.lower()).ratio() for x in argList])
-                    maxVal = np.max(similarity)
-                    maxId = np.argmax(similarity)
-                    #if(maxVal>0.8):
-                    raise AttributeError('Key-word argument "{}" not understood. Did you mean "{}"?'.format(key,argList[maxId]))
-                    #else:
-                    #    raise AttributeError('Key-word argument "{}" not understood?'.format(key))
-                    
-        argList = newFunc._original.__code__.co_varnames
-        checkArgumentList(argList,kwargs)
-        returnval = func(self,*args,**kwargs)
-        return returnval
-
-    for attr in ['__module__', '__name__', '__qualname__', '__doc__', '__annotations__','__weakref__']:
-        try:
-            value = getattr(func, attr)
-        except AttributeError:
-            pass
+def checkArgumentList(argList,kwargs):
+    notFound = []
+    for key in kwargs:
+        if key not in argList:
+            similarity = np.array([SequenceMatcher(None, key.lower(), x.lower()).ratio() for x in argList])
+            maxVal = np.max(similarity)
+            maxId = np.argmax(similarity)
+            notFound.append('Key-word argument "{}" not understood. Did you mean "{}"?'.format(key,argList[maxId]))
+    if len(notFound)>0:
+        if len(notFound)>1:
+            errorMsg = 'The following key-word arguments are not understood:\n'
+            errorMsg+='\n'.join(notFound)
         else:
-            setattr(newFunc, attr, value)
-    for attr in ['__dict__']:
-        getattr(newFunc, attr).update(getattr(func, attr, {}))
-    newFunc._original = func
-    return newFunc
-
+            errorMsg = notFound[0]
+        raise AttributeError(errorMsg)
 
 def my_timer_N(N=0): # pragma: no cover
     """Timer function to measure time consumbtion of function.
