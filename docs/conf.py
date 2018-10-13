0a885d931b7e2f68f80a8a092f6767d3744ad691
Jakob Lass <lass.jakob@gmail.com>
2018-10-13 12:41:31 +0200

    Update to version 0.6.0

--- docs/conf.py
+++ docs/conf.py
@@ -31,10 +31,10 @@ copyright = u'2018, Jakob Lass'
 author = u'Jakob Lass'
 
 # The short X.Y version
-version = u'0.5.2'
+version = u'0.6.0'
 # The full version, including alpha/beta/rc tags
 
-release = u'0.5.2'
+release = u'0.6.0'
 
 
 
