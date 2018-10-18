c9fa502c88ff4a31a7b4e9a1177bb0e140f50db8
Jakob Lass <lass.jakob@gmail.com>
2018-10-18 15:00:08 +0200

    Add logging decorator for classes

--- MJOLNIR/_tools.py
+++ MJOLNIR/_tools.py
@@ -3,6 +3,7 @@ import sys
 import numpy as np
 from difflib import SequenceMatcher
 import functools
+import logging
 
 def KwargChecker(function=None,include=None):
     """Function to check if given key-word is in the list of accepted Kwargs. If not directly therein, checks capitalization. If still not match raises error
@@ -20,20 +21,29 @@ def KwargChecker(function=None,include=None):
     def KwargCheckerNone(func):
         @functools.wraps(func)
         def newFunc(self,*args,**kwargs):
-            N = func.__code__.co_argcount            
-            argList = list(newFunc._original.__code__.co_varnames[:N])
-            if not function is None:
-                for arg in function.__code__.co_varnames[:function.__code__.co_argcount]:
-                    argList.append(str(arg))
-            if not include is None:
-                for arg in include:
-                    argList.append(str(arg))
+            argList = extractArgsList(func,newFunc,function,include)
             checkArgumentList(argList,kwargs)
             returnval = func(self,*args,**kwargs)
             return returnval
+        #newFunc.__dict__ =  func.__dict__
         newFunc._original = func
+        newFunc._include = include
+        newFunc._function = function
+        
+        
         return newFunc
     return KwargCheckerNone
+
+def extractArgsList(func,newFunc,function,include):
+    N = func.__code__.co_argcount            
+    argList = list(newFunc._original.__code__.co_varnames[:N])
+    if not function is None:
+        for arg in function.__code__.co_varnames[:function.__code__.co_argcount]:
+            argList.append(str(arg))
+    if not include is None:
+        for arg in include:
+            argList.append(str(arg))
+    return argList
 
 def checkArgumentList(argList,kwargs):
     notFound = []
@@ -49,7 +59,8 @@ def checkArgumentList(argList,kwargs):
             errorMsg+='\n'.join(notFound)
         else:
             errorMsg = notFound[0]
-        raise AttributeError(errorMsg)
+        error = AttributeError(errorMsg)
+        raise error
 
 def my_timer_N(N=0): # pragma: no cover
     """Timer function to measure time consumbtion of function.
@@ -83,3 +94,104 @@ def my_timer_N(N=0): # pragma: no cover
             return returnval
         return newFunc
     return my_timer
+
+def beautifyArgs(args=(),kwargs={}):
+    """Beautify arguments and keyword arguments. Returns formated string with arguments and 
+    keyword argumenets seperated with commas as called in a function"""
+    returnStr = ''
+    if not args == ():
+        args = list(args)
+        returnStr =', '.join([str(x) if type(x)!=str else '"{}"'.format(x) for x in args])
+        if not kwargs == {}:
+            returnStr+=', '
+    if not kwargs == {}:
+        kwformat = ['{}={}'.format(key,kwargs[key]) if type(kwargs[key])!=str else '{}="{}"'.format(key,kwargs[key])  for key in kwargs]
+        returnStr+=', '.join([str(x) for x in kwformat])
+    return returnStr
+
+def createLogger(self,name,stream=sys.stdout,level=logging.ERROR):
+    self._log =  logging.getLogger(name)
+    self._log.setLevel(level)
+    ch = logging.StreamHandler(sys.stdout)
+    ch.setLevel(level)
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    ch.setFormatter(formatter)
+    self._log.addHandler(ch)
+
+def logMethod(self,original_function): 
+    @functools.wraps(original_function)     
+    def new_function(*args,**kwargs):
+        self._log.info('Calling {}({})'.format(new_function.original_function.__name__,beautifyArgs(args,kwargs)))
+        try:
+            x = original_function(*args,**kwargs)
+        except Exception as WrapperError:
+            self._log.error('***** FAILED *****')
+            raise WrapperError
+        return x    
+    new_function.original_function= original_function                                   
+    return new_function  
+
+def logAttribute(self,original_attribute):      
+    self._log.info('Calling attribute {}'.format(str(original_attribute)))                                     
+    
+
+def logClass(parent=None,log=__name__,stream=sys.stdout,level=logging.CRITICAL):
+    if parent is None:
+        parent = object
+    def track_all_class_methods(Cls):
+        class NewCls(parent):
+            
+            def __init__(self,*args,**kwargs):
+                createLogger(self,name=log,stream=stream,level=level)
+                self._log.info('Calling {}({})'.format(Cls.__name__,beautifyArgs(args,kwargs)))
+                self._Instance = Cls(*args,**kwargs)
+
+                
+            def __getattribute__(self,s):
+                """
+                this is called whenever any attribute of a NewCls object is accessed. This function first tries to 
+                get the attribute off NewCls. If it fails then it tries to fetch the attribute from self.oInstance (an
+                instance of the decorated class). If it manages to fetch the attribute from self.oInstance, and 
+                the attribute is an instance method then `time_this` is applied.
+                """
+                try:    
+                    x = super(NewCls,self).__getattribute__(s)
+                    skip = True
+                except AttributeError:      
+                    x = self._Instance.__getattribute__(s)
+                    skip = False
+                else:
+                    pass
+                finally:
+                    if skip and s[0]!='_':
+                        if type(x) == type(self.__init__):# and x != self.__init__: # it is an instance method
+                            return logMethod(self,x)                 # this is equivalent of just decorating the method with logMethod
+                            pass
+                        else:
+                            logAttribute(self,s)
+                    return x
+
+            def _printLog(self):
+                pass
+                #print('\n'.join([x for x in self._log]))
+        d = Cls.__dict__
+        keys = d.keys()
+        copy = [x for x in list(Cls.__dict__) if not x in list(NewCls.__dict__)]
+        
+        for key in copy+['__doc__']:
+            if key=='__dict__' or key in ['__getattribute__','__init__']: #  Check if the value in dict is a function
+                continue
+            
+            if type(d[key])==type(Cls.__init__):
+                if hasattr(d[key],'_original'):
+
+                    setattr(NewCls, key, KwargChecker(d[key]._function,d[key]._include)(d[key]._original))
+                else:
+                    setattr(NewCls, key, d[key])
+                    for funckey in d[key].__dict__:
+                        setattr(d[key],funckey,d[key].__dict__[funckey])
+                        
+            else:
+                setattr(NewCls, key, d[key])
+        return NewCls
+    return track_all_class_methods
