55c188cec86f6f9b5a77d456d5270c9fba7487cb
Jakob Lass <lass.jakob@gmail.com>
2018-10-24 16:38:48 +0200

    Remove test

--- Tutorials/Generate_normalization.py
+++ Tutorials/Generate_normalization.py
@@ -2,7 +2,7 @@ import sys,os
 sys.path.append('/home/lass/Dropbox/PhD/Software/MJOLNIR/')
 
 from MJOLNIR.Geometry import Instrument
-def test_Generate_normalization(plot=False):
+def Generate_normalization(plot=False):
     Instr = Instrument.Instrument(fileName='TestData/1024/CAMEA_Full.xml')
     Instr.initialize()
 
@@ -10,4 +10,4 @@ def test_Generate_normalization(plot=False):
     Instr.generateCalibration(Vanadiumdatafile=VanNormFile,savelocation='TestData/test_',plot=plot,tables=[8]) 
 
 if __name__ == '__main__':
-    test_Generate_normalization(True)
+    Generate_normalization(True)
