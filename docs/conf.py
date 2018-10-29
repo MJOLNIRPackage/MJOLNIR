594f8a521168656e2f521720bbb376c31933dbcf
Jakob Lass <lass.jakob@gmail.com>
2018-10-29 11:33:29 +0100

    Update sphinx requirenment to 1.7

--- docs/conf.py
+++ docs/conf.py
@@ -43,7 +43,7 @@ release = u'0.7'
 
 # If your documentation needs a minimal Sphinx version, state it here.
 #
-needs_sphinx = '1.8'
+needs_sphinx = '1.7'
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
