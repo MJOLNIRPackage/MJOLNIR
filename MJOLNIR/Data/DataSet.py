17326b0952bf6437ebb452d442f02292fed33899
Jakob Lass <lass.jakob@gmail.com>
2019-01-07 11:54:20 +0100

    Add kwarg of axis to Vie3D method

--- MJOLNIR/Data/DataSet.py
+++ MJOLNIR/Data/DataSet.py
@@ -1443,7 +1443,7 @@ class DataSet(object):
         return cut1DE(positions = positions, I=I, Norm=Norm,Monitor=Monitor,E1=E1,E2=E2,q=Q,width=width,minPixel=minPixel)
 
     @_tools.KwargChecker()
-    def View3D(self,dx,dy,dz,rlu=True, log=False,grid=False):
+    def View3D(self,dx,dy,dz,rlu=True, log=False,grid=False,axis=2):
         """View data in the Viewer3D object. 
 
         Args:
@@ -1459,6 +1459,8 @@ class DataSet(object):
             - rlu (Bool): If true a rlu axis is used for plotting orthervise qx,qy (Default True).
 
             - log (Bool): If true logarithm of intensity is plotted
+
+            - axis (int): Axis shown initially (default 2)
 
         If one plots not using RLU, everything is plotted in real units (1/AA), and the Qx and QY is not rotated. That is, the
         x axis in energy is not along the projection vector. The cuts of constant Qx and Qy does not represent any symmetry directions in
@@ -1487,7 +1489,7 @@ class DataSet(object):
         warnings.simplefilter('ignore')
         Intensity = np.divide(Data[0]*Data[3],Data[1]*Data[2])
         warnings.simplefilter('once')
-        Viewer = Viewer3D.Viewer3D(Data,bins,axis=2,ax=axes,grid=grid,log=log)
+        Viewer = Viewer3D.Viewer3D(Data,bins,axis=axis,ax=axes,grid=grid,log=log)
         return Viewer
 
     def convertToQxQy(self,HKL):
