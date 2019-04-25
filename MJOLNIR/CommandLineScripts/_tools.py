5f2dcf185061e3b6206bbe0cef7cebdea7ca9279
Jakob Lass <lass.jakob@gmail.com>
2019-04-25 09:30:26 +0200

    If no .settings file exist, create empty dict and save it

--- MJOLNIR/CommandLineScripts/_tools.py
+++ MJOLNIR/CommandLineScripts/_tools.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import pickle
 
 settingsFile = os.path.join(*[x for x in os.path.split(__file__)[:-1]])
 settingsFile = os.path.join(settingsFile,'.settings')
@@ -11,41 +12,23 @@ fileFormats = rawFileFormats+convertedFileFormats
 
 
 def loadSetting(string):
-    returnVal=None
     if Exists():
-        with open(os.path.realpath(settingsFile),'r') as f:
-            lines = f.readlines()
-            returnVal = None
-            for l in lines:
-                if string in l.split(':')[0]:
-                    returnVal = l.split(':')[-1].strip()
-                    if returnVal[0] in ['(','[']:
-                        returnVal=tuple([x[1:-1] for x in str(returnVal)[1:-1].split(', ')])
-    return returnVal
+        with open(settingsFile,"rb") as pickle_in:
+            loaded_dict = pickle.load(pickle_in)
+        returnValue = loaded_dict[string]
+        return returnValue
+    else:
+        return None
 
 def updateSetting(name,value):
     if Exists():
-        os.rename(settingsFile,settingsFile+'Old')
-        lineWritten=False
-        with open(settingsFile,'w') as newF:
-            with open(settingsFile+'Old','r') as oldF:
-                for line in oldF:
-                    if line.split(':')[0]==name:
-                        newF.write(name+':'+str(value)+'\n')
-                        lineWritten = True
-                    else:
-                        newF.write(line)
-                if not lineWritten:
-                    newF.write(name+':'+str(value)+'\n')
-        os.remove(settingsFile+'Old')
+        with open(settingsFile,"rb") as pickle_in:
+            loaded_dict = pickle.load(pickle_in)
     else:
-        with open(os.path.realpath(settingsFile),'w') as f:
-            f.write(name+':'+value+'\n')
+        loaded_dict = {}
+    loaded_dict[name]=value
+    with open(settingsFile,"wb") as pickle_out:
+        pickle.dump(loaded_dict, pickle_out)
 
 def Exists(file = settingsFile):
-    if not os.path.isfile(settingsFile):
-        with open(os.path.realpath(settingsFile),'w') as f:
-            f.write('')
-        return False
-    else:
-        return True
+    return os.path.isfile(settingsFile)
