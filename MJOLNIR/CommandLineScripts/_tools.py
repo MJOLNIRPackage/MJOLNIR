0e3098dff9e38819c0d65f242cd33a383483cabb
Jakob Lass <lass.jakob@gmail.com>
2019-04-26 09:16:24 +0200

    Create and Utilize file loader

--- MJOLNIR/CommandLineScripts/_tools.py
+++ MJOLNIR/CommandLineScripts/_tools.py
@@ -1,13 +1,16 @@
 import os
 import sys
 import pickle
+import re
+import MJOLNIR._tools
+import numpy as np
 
 settingsFile = os.path.join(*[x for x in os.path.split(__file__)[:-1]])
 settingsFile = os.path.join(settingsFile,'.settings')
 
-rawFileFormats = ['.hdf']
-convertedFileFormats = ['.nxs']
-fileFormats = rawFileFormats+convertedFileFormats
+rawFileFormats = ' '.join([x for x in ['.hdf']])
+convertedFileFormats = ' '.join([x for x in ['*.nxs']])
+fileFormats = ' '.join([x for x in [rawFileFormats,convertedFileFormats]])
 
 
 
@@ -15,7 +18,10 @@ def loadSetting(string):
     if Exists():
         with open(settingsFile,"rb") as pickle_in:
             loaded_dict = pickle.load(pickle_in)
-        returnValue = loaded_dict[string]
+        if string in loaded_dict:
+            returnValue = loaded_dict[string]
+        else:
+            returnValue = None
         return returnValue
     else:
         return None
@@ -32,3 +38,60 @@ def updateSetting(name,value):
 
 def Exists(file = settingsFile):
     return os.path.isfile(settingsFile)
+
+def extractDataFiles(args,settingsName,oneFile = False):
+    if not 'DataFile' in args:
+        startingPath = loadSetting(settingsName)
+        if not startingPath is None:
+            if isinstance(startingPath,tuple):
+                startingDir = '/'.join([x for x in list(startingPath)[0].split('/')[:-1]])
+            else:
+                startingDir = startingPath
+        else:
+            startingDir = None
+        
+        if 'reuse' in args:
+            reuse = args.reuse
+        else:
+            reuse = False
+
+        if reuse:
+            files = startingPath
+        else:
+            try:
+                import tkinter as tk
+                from tkinter import filedialog
+            except:
+                import Tkinter as tk
+                import tkFileDialog as filedialog
+            
+            root = tk.Tk()
+            root.withdraw()
+            if oneFile:
+                files = filedialog.askopenfilename(initialdir=startingDir, title = 'Select file',filetypes=(('Data files',fileFormats),))#('All Files','*')))
+            else:
+                files = filedialog.askopenfilenames(initialdir=startingDir, title = 'Select file(s)',filetypes=(('Data files',fileFormats),))#('All Files','*')))
+
+        files = tuple(np.unique(files))
+        if len(list(files))==0: # No file chosen
+            sys.exit()
+
+        directory = os.path.split(files[0])[0]
+
+
+    else:
+        pattern = re.compile(r'^(\d+[-,]?){1,}\d') # Generate pattern to recognize format: Start with digits, and then - or . or nothing repeated at least once, and ending in digit.
+        res = pattern.match(args.DataFile[0])
+        if not res is None:
+            if res.start()==0 and res.end() == len(args.DataFile[0]): # Correct expression is given
+                if len(args.DataFile) == 3: # Provided is string, directory and year
+                    fileNumbers, directory, year = args.DataFile
+                    year = int(year)
+                    files = MJOLNIR._tools.fileListGenerator(fileNumbers,directory,year)
+                elif len(args.DataFile) == 4:
+                    raise NotImplementedError('Using non-CAMEA input is currently not supported... sorry :-/ ')
+                else:
+                    raise AttributeError('Provided data file arguments does not fit the scheme of being number list, directory, and year! (+ possibly instrument)')
+        else:
+            files = args.DataFile
+    return files
