21f0ca72bd38b9a4e28c8dc1279992d260a52f4f
Jakob Lass <lass.jakob@gmail.com>
2019-05-27 16:06:21 +0200

    Set aspect ratio only in 2D Change multiplyer function in numpy

--- MJOLNIR/Data/DataSet.py
+++ MJOLNIR/Data/DataSet.py
@@ -470,7 +470,7 @@ class DataSet(object):
             q1,q2 = self.convertToQxQy([q1,q2])
             # Rotate all data files to fit with first data file
             thetaDifference = [s.theta-samples[0].theta for s in samples]
-            rotationMatrices = [samples[0].RotMat.T@s.RotMat for s in samples]#[_tools.Rot(theta,deg=False) for theta in thetaDifference]
+            rotationMatrices = [np.multiply(samples[0].RotMat.T,s.RotMat) for s in samples]#[_tools.Rot(theta,deg=False) for theta in thetaDifference]
             Q = [[QX,QY] for QX,QY in zip(np.split(qx,maskIndices),np.split(qy,maskIndices))]
             qx,qy = np.concatenate([np.einsum('ij,j...->i...',rot,q) for rot,q in zip(rotationMatrices,Q)],axis=1)
 
@@ -693,7 +693,7 @@ class DataSet(object):
             q1,q2 = self.convertToQxQy([q1,q2])
             # Rotate all data files to fit with first data file
             thetaDifference = [s.theta-samples[0].theta for s in samples]
-            rotationMatrices = [samples[0].RotMat.T@s.RotMat for s in samples]#[_tools.Rot(theta,deg=False) for theta in thetaDifference]
+            rotationMatrices = [np.multiply(samples[0].RotMat.T,s.RotMat) for s in samples]#[_tools.Rot(theta,deg=False) for theta in thetaDifference]
             Q = [[QX,QY] for QX,QY in zip(np.split(qx,maskIndices),np.split(qy,maskIndices))]
             qx,qy = np.concatenate([np.einsum('ij,j...->i...',rot,q) for rot,q in zip(rotationMatrices,Q)],axis=1)
         positions = np.array([qx,qy,energy])
@@ -771,7 +771,7 @@ class DataSet(object):
             q1,q2 = self.convertToQxQy([q1,q2])
             # Rotate all data files to fit with first data file
             thetaDifference = [s.theta-samples[0].theta for s in samples]
-            rotationMatrices = [samples[0].RotMat.T@s.RotMat for s in samples]#[_tools.Rot(theta,deg=False) for theta in thetaDifference]
+            rotationMatrices = [np.multiply(samples[0].RotMat.T,s.RotMat) for s in samples]#[_tools.Rot(theta,deg=False) for theta in thetaDifference]
             Q = [[QX,QY] for QX,QY in zip(np.split(qx,maskIndices),np.split(qy,maskIndices))]
             qx,qy = np.concatenate([np.einsum('ij,j...->i...',rot,q) for rot,q in zip(rotationMatrices,Q)],axis=1)
 
@@ -1172,8 +1172,9 @@ class DataSet(object):
                 pmeshs.append(ax.contourf3D(QX,QY,I,zdir = 'z',offset=np.mean(EBins[i:i+2]),levels=levels,cmap=cmap,**kwargs))
             else:
                 pmeshs.append(ax.pcolormesh(Qx[i],Qy[i],Int[i],zorder=zorder,cmap=cmap,**kwargs))
-        ax.set_aspect('equal')
-        ax.grid(True, zorder=0)
+        if not _3D:
+            ax.set_aspect('equal')
+            ax.grid(True, zorder=0)
         def set_clim(pmeshs,vmin,vmax):
             for pmesh in pmeshs:
                 pmesh.set_clim(vmin,vmax)
