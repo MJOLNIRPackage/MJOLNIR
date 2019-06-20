75506eebf4c1355d1b48476b5678c2f13f808f87
Jakob Lass <lass.jakob@gmail.com>
2019-06-20 13:44:43 +0200

    decrement version to allow new version to be created

--- MJOLNIR/__init__.py
+++ MJOLNIR/__init__.py
@@ -2,11 +2,10 @@
 MJOLNIR Module
 ^^^^^^^^^^^^^^
 
-Further documentation? Version is 1.0.10?
 """
 #import Geometry, Statistic, Data
 
 import sys,os
 sys.path.append('.')
 
-__version__ = '1.1.3'
+__version__ = '1.1.2'
