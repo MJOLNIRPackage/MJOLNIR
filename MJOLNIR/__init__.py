26b8ec996c244542249d3fc5169deb93cb154728
Jakob Lass <lass.jakob@gmail.com>
2020-04-18 10:33:02 +0200

    Update version

--- MJOLNIR/__init__.py
+++ MJOLNIR/__init__.py
@@ -8,7 +8,7 @@ MJOLNIR Module
 import sys,os
 sys.path.append('.')
 
-__version__ = '1.1.6'
+__version__ = '1.1.7'
 __author__ = 'Jakob Lass'
 
 
--- docs/conf.py
+++ docs/conf.py
@@ -31,9 +31,9 @@ copyright = u'2019, Jakob Lass'
 author = u'Jakob Lass'
 
 # The short X.Y version
-version = u'1.1.6'
+version = u'1.1.7'
 # The full version, including alpha/beta/rc tags
-release = u'1.1.6'
+release = u'1.1.7'
 
 
 
