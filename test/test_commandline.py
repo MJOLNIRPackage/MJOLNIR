86f7c4da091c0772b71a180ec9e36ba98a3d6ed5
Jakob Lass <lass.jakob@gmail.com>
2021-03-23 10:54:06 +0100

    Update test to (hopefully) remove error for windows 10 + pyhton3.9

--- test/test_commandline.py
+++ test/test_commandline.py
@@ -27,14 +27,15 @@ def test_CalibrationInspector_Help_Text():
     helpTextWin10 = b'usage: MJOLNIRCalibrationInspector.exe [-h] [-s SAVE]\r\n                                       [-p [PLOTLIST [PLOTLIST ...]]]\r\n                                       [-b BINNING]\r\n                                       [DataFile]\r\n\r\nInspection tool to visualize calibration tables in a data file.\r\n\r\npositional arguments:\r\n  DataFile              Data file from which calibration table is to be\r\n                        plotted. If none provided file dialogue will appear.\r\n\r\noptional arguments:\r\n  -h, --help            show this help message and exit\r\n  -s SAVE, --save SAVE  Location to which the generated file will be saved.\r\n  -p [PLOTLIST [PLOTLIST ...]], --plot [PLOTLIST [PLOTLIST ...]]\r\n                        List of wanted plots to be generated. Should be\r\n                        "A4","Normalization","Ef","EfOverview". Default all of\r\n                        them.\r\n  -b BINNING, --binning BINNING\r\n                        Binning to be inspected. Default \'8\'\r\n'
     helpTextPython39 = b'usage: MJOLNIRCalibrationInspector.exe [-h] [-s SAVE] [-p [PLOTLIST ...]]\r\n                                       [-b BINNING]\r\n                                       [DataFile]\r\n\r\nInspection tool to visualize calibration tables in a data file.\r\n\r\npositional arguments:\r\n  DataFile              Data file from which calibration table is to be\r\n                        plotted. If none provided file dialogue will appear.\r\n\r\noptional arguments:\r\n  -h, --help            show this help message and exit\r\n  -s SAVE, --save SAVE  Location to which the generated file will be saved.\r\n  -p [PLOTLIST ...], --plot [PLOTLIST ...]\r\n                        List of wanted plots to be generated. Should be\r\n                        "A4","Normalization","Ef","EfOverview". Default all of\r\n                        them.\r\n  -b BINNING, --binning BINNING\r\n                        Binning to be inspected. Default \'8\'\r\n'
     helpTextTravisPython39 = b'usage: MJOLNIRCalibrationInspector [-h] [-s SAVE] [-p [PLOTLIST ...]]\n                                   [-b BINNING]\n                                   [DataFile]\n\nInspection tool to visualize calibration tables in a data file.\n\npositional arguments:\n  DataFile              Data file from which calibration table is to be\n                        plotted. If none provided file dialogue will appear.\n\noptional arguments:\n  -h, --help            show this help message and exit\n  -s SAVE, --save SAVE  Location to which the generated file will be saved.\n  -p [PLOTLIST ...], --plot [PLOTLIST ...]\n                        List of wanted plots to be generated. Should be\n                        "A4","Normalization","Ef","EfOverview". Default all of\n                        them.\n  -b BINNING, --binning BINNING\n                        Binning to be inspected. Default \'8\'\n'
-#helpTextPython39.decode().replace('\r','').encode()#b'usage: MJOLNIRCalibrationInspector [-h] [-s SAVE] [-p [PLOTLIST ...]]\n                                   [-b BINNING]\n                                   [DataFile]\n\nInspection tool to visualize calibration tables in a data file.\n\npositional arguments:\n  DataFile              Data file from which calibration table is to be\n                        plotted. If none provided file dialogue will appear.\n\noptional arguments:\n  -h, --help            show this help message and exit\n  -s SAVE, --save SAVE  Location to which the generated file will be saved.\n  -p [PLOTLIST ...], --plot [PLOTLIST ...]\n                        List of wanted plots to be generated. Should be\n                        "A4","Normalization","Ef","EfOverview". Default all of\n                        them.\n  -b BINNING, --binning BINNING\n                        Binning to be inspected. Default \'8\'\n'
-    
+
+    helpTextPython39Windows =  b'usage: MJOLNIRCalibrationInspector [-h] [-s SAVE] [-p [PLOTLIST ...]]\r\n                                   [-b BINNING]\r\n                                   [DataFile]\r\n\r\nInspection tool to visualize calibration tables in a data file.\r\n\r\npositional arguments:\r\n  DataFile              Data file from which calibration table is to be\r\n                        plotted. If none provided file dialogue will appear.\r\n\r\noptional arguments:\r\n  -h, --help            show this help message and exit\r\n  -s SAVE, --save SAVE  Location to which the generated file will be saved.\r\n  -p [PLOTLIST ...], --plot [PLOTLIST ...]\r\n                        List of wanted plots to be generated. Should be\r\n                        "A4","Normalization","Ef","EfOverview". Default all of\r\n                        them.\r\n  -b BINNING, --binning BINNING\r\n                        Binning to be inspected. Default \'8\'\r\n'
     helpTextWin32Bool = result == helpTextWin32
     helpTextLinuxBool = result == helpTextLinux
     helpTextWin10Bool = result == helpTextWin10
     helpTextPython39Bool = result == helpTextPython39
     helpTextTravisPython39Bool = result == helpTextTravisPython39
-    assert(helpTextWin32Bool or helpTextLinuxBool or helpTextWin10Bool or helpTextPython39Bool or helpTextTravisPython39Bool)
+    helpTextPython39WindowsBool = result == helpTextPython39Windows
+    assert(helpTextWin32Bool or helpTextLinuxBool or helpTextWin10Bool or helpTextPython39Bool or helpTextTravisPython39Bool or helpTextPython39WindowsBool)
 
 
 @pytest.mark.skip(reason="Fails on the headless Travis-ci")
@@ -67,6 +68,7 @@ def test_History_Help_Text():
     helpTextPython39Windows = b'usage: MJOLNIRHistory.exe [-h] [-s SAVE] [-r] [DataFile ...]\r\n\r\nHistory tool for displaying files and command for selected data files.\r\n\r\npositional arguments:\r\n  DataFile              Data file(s) to be used. If none provided file\r\n                        dialogue will appear. Using string format, directory\r\n                        and year is also possible. See documentation.\r\n\r\noptional arguments:\r\n  -h, --help            show this help message and exit\r\n  -s SAVE, --save SAVE  Location to which the generated history will be saved.\r\n  -r, --reuse           Set flag to reuse files from previous usage. Default\r\n                        false.\r\n'
     helpTextTravisPython39Windows = helpTextPython39Windows.decode().replace('\r','').encode()
     helpTextTravisPython39 = helpTextTravisPython39Windows.decode().replace('.exe','').encode()
+    helpTextTravisMoreTest = helpTextPython39Windows.decode().replace('.exe','').encode()
 
     helpTextWin32Bool = result == helpTextWin32
     helpTextLinuxBool = result == helpTextLinux
@@ -74,7 +76,8 @@ def test_History_Help_Text():
     helpTextPython39Bool = result == helpTextPython39Windows
     helpTextTravisPython39WindowsBool = result == helpTextTravisPython39Windows
     helpTextTravisPython39Bool = result == helpTextTravisPython39
-    assert(helpTextWin32Bool or helpTextLinuxBool or helpTextWin10Bool or helpTextPython39Bool or helpTextTravisPython39WindowsBool or helpTextTravisPython39Bool)
+    helpTextTravisMoreTestBool = result == helpTextTravisMoreTest
+    assert(helpTextWin32Bool or helpTextLinuxBool or helpTextWin10Bool or helpTextPython39Bool or helpTextTravisPython39WindowsBool or helpTextTravisPython39Bool or helpTextTravisMoreTestBool)
 
 
 def test_History_SingleFile():
@@ -111,6 +114,7 @@ def test_Convert_Help_Text():
     helpTextPython39Windows = b"usage: MJOLNIRConvert.exe [-h] [-s SAVE] [-b BINNING] [-r] [DataFile ...]\r\n\r\nConversion tool for converting output h5 files to nxs files.\r\n\r\npositional arguments:\r\n  DataFile              Data file(s) to be used. If none provided file\r\n                        dialogue will appear. Using string format, directory\r\n                        and year is also possible. See documentation.\r\n\r\noptional arguments:\r\n  -h, --help            show this help message and exit\r\n  -s SAVE, --save SAVE  Location to which the generated file will be saved.\r\n  -b BINNING, --binning BINNING\r\n                        Binning performed. Default '8'\r\n  -r, --reuse           Set flag to reuse files from previous usage. Default\r\n                        false.\r\n"
     helpTextTravisPython39Windows = helpTextPython39Windows.decode().replace('\r','').encode()
     helpTextTravisPython39 = helpTextTravisPython39Windows.decode().replace('.exe','').encode()
+    helpTextTravisMoreTest = helpTextPython39Windows.decode().replace('.exe','').encode()
 
     helpTextWin32Bool = result == helpTextWin32
     helpTextLinuxBool = result == helpTextLinux
@@ -118,7 +122,8 @@ def test_Convert_Help_Text():
     helpTextPython39Bool = result == helpTextPython39Windows
     helpTextTravisPython39WindowsBool = result == helpTextTravisPython39Windows
     helpTextTravisPython39Bool = result == helpTextTravisPython39
-    assert(helpTextWin32Bool or helpTextLinuxBool or helpTextWin10Bool or helpTextPython39Bool or helpTextTravisPython39WindowsBool or helpTextTravisPython39Bool)
+    helpTextTravisMoreTestBool = result == helpTextTravisMoreTest
+    assert(helpTextWin32Bool or helpTextLinuxBool or helpTextWin10Bool or helpTextPython39Bool or helpTextTravisPython39WindowsBool or helpTextTravisPython39Bool or helpTextTravisMoreTestBool)
 
 
     
@@ -164,6 +169,7 @@ def test_3DView_Help_Text():
     helpTextWin10 = b'usage: MJOLNIR3DView.exe [-h] [-r] [-b BINNING]\r\n                         [-d DQXDQYDE DQXDQYDE DQXDQYDE] [-M VMAX] [-m VMIN]\r\n                         [DataFile [DataFile ...]]\r\n\r\nConversion tool for quick visualization using the viewer3D.\r\n\r\npositional arguments:\r\n  DataFile              Data file(s) to be used. If none provided file\r\n                        dialogue will appear. Using string format, directory\r\n                        and year is also possible. See documentation.\r\n\r\noptional arguments:\r\n  -h, --help            show this help message and exit\r\n  -r, --reuse           Set flag to reuse files from previous usage. Default\r\n                        false.\r\n  -b BINNING, --binning BINNING\r\n                        Binning performed. Default \'8\'\r\n  -d DQXDQYDE DQXDQYDE DQXDQYDE, --dQxdQydE DQXDQYDE DQXDQYDE DQXDQYDE\r\n                        Binning used to plot in 3D, Default [0.03,0.03,0.08]\r\n  -M VMAX, --VMax VMAX  Maximal value for plotting, default max of data\r\n  -m VMIN, --VMin VMIN  Minimal value for plotting, default min of data\r\n'
     helpTextPython39 = b"usage: MJOLNIR3DView [-h] [-r] [-b BINNING] [-d DQXDQYDE DQXDQYDE DQXDQYDE]\n                     [-M VMAX] [-m VMIN]\n                     [DataFile ...]\n\nConversion tool for quick visualization using the viewer3D.\n\npositional arguments:\n  DataFile              Data file(s) to be used. If none provided file\n                        dialogue will appear. Using string format, directory\n                        and year is also possible. See documentation.\n\noptional arguments:\n  -h, --help            show this help message and exit\n  -r, --reuse           Set flag to reuse files from previous usage. Default\n                        false.\n  -b BINNING, --binning BINNING\n                        Binning performed. Default '8'\n  -d DQXDQYDE DQXDQYDE DQXDQYDE, --dQxdQydE DQXDQYDE DQXDQYDE DQXDQYDE\n                        Binning used to plot in 3D, Default [0.03,0.03,0.08]\n  -M VMAX, --VMax VMAX  Maximal value for plotting, default max of data\n  -m VMIN, --VMin VMIN  Minimal value for plotting, default min of data\n"
     helpTextTravisPython39 = helpTextPython39.decode().replace('\r','').encode()
+    helpTextTravisMoreTest = helpTextPython39Windows.decode().replace('.exe','').encode()
 
 
     helpTextWin32Bool = result == helpTextWin32
@@ -171,7 +177,8 @@ def test_3DView_Help_Text():
     helpTextWin10Bool = result == helpTextWin10
     helpTextPython39Bool = result == helpTextPython39
     helpTextTravisPython39Bool = result == helpTextTravisPython39
-    assert(helpTextWin32Bool or helpTextLinuxBool or helpTextWin10Bool or helpTextPython39Bool or helpTextTravisPython39Bool)
+    helpTextTravisMoreTestBool = result == helpTextTravisMoreTest
+    assert(helpTextWin32Bool or helpTextLinuxBool or helpTextWin10Bool or helpTextPython39Bool or helpTextTravisPython39Bool or helpTextTravisMoreTestBool)
 
 
   
