199caa8dc4e2490121ddfa7ae1bbef36cf847ce5
Jakob Lass <lass.jakob@gmail.com>
2021-04-13 12:10:43 +0200

    Moving of symbols and molecular calculation (#61)
    * Reorganize import and lists
    
    * Update to break on Travis
    
    * Create help text string from helpTextPython39
    
    * Reorganize test
    Update to replace \r in travis specific string
    
    * Update all cmd help tests
    
    * Add print statement for temporary debugging
    
    * Update help texts
    
    * Update test to (hopefully) remove error for windows 10 + pyhton3.9
    
    * Catch typo
    
    * Add print for debut...
    
    * Update help string
    
    * Restrict matplotlib to version <3.4
    Add python 3.9 to classifiers
    
    * Update version

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ pythonPath =  os.path.relpath(os.path.join(installFolder,'MJOLNIR'),sys.base_pre
 setup(
     name='MJOLNIR',
 
-    version='1.1.17',
+    version='1.1.18',
     description=('Neutron Scattering software suite.'),
     long_description=long_description,
     author='Jakob Lass',
@@ -39,7 +39,7 @@ setup(
                             'MJOLNIR3DView = MJOLNIR.CommandLineScripts.MJOLNIR3DView:main']
         },
     python_requires='>=3.5' if not operatingSystem == 'darwin' else '>=3.6',
-    install_requires=['matplotlib>=3','numpy>=1.14','h5py>=2.5','scipy','datetime','pytest>=4.6','pyperclip','decorator','pandas','future',
+    install_requires=['matplotlib>=3,<3.4','numpy>=1.14','h5py>=2.5','scipy','datetime','pytest>=4.6','pyperclip','decorator','pandas','future',
                     'pip>=20','ufit>=1.4.0','pyqtgraph','regex'], # ,'ufit','sip','PyQt5-sip','PyQt5<=5.12'
     classifiers=[
         'Development Status :: 3 - Alpha',
@@ -52,5 +52,6 @@ setup(
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8'],
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9'],
     )
