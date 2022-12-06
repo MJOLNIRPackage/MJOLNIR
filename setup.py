e00ecca97dbfdf5e1a9bd47641557d2b4220352c
Jakob Lass <lass.jakob@gmail.com>
2022-12-06 13:04:17 +0100

    Update version

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ pythonPath =  os.path.relpath(os.path.join(installFolder,'MJOLNIR'),sys.base_pre
 
 setup(
     name='MJOLNIR',
-    version='1.2.3',
+    version='version=1.2.4',
     description=('Neutron Scattering software suite.'),
     long_description=long_description,
     author='Jakob Lass',
