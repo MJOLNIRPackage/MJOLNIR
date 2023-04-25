a0a6ac1b000ffedaeef4488cc6da29a1796bb810
Jakob Lass <lass.jakob@gmail.com>
2023-04-25 16:23:32 +0200

    Update to version 1.2.6post2

--- docs/conf.py
+++ docs/conf.py
@@ -31,9 +31,9 @@ copyright = u'2020, Jakob Lass'
 author = u'Jakob Lass'
 
 # The short X.Y version
-version = u'1.2.6post1'
+version = u'1.2.6post2'
 # The full version, including alpha/beta/rc tags
-release = u'1.2.6post1'
+release = u'1.2.6post2'
 
 
 
--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ pythonPath =  os.path.relpath(os.path.join(installFolder,'MJOLNIR'),sys.base_pre
 
 setup(
     name='MJOLNIR',
-    version='1.2.6post1',
+    version='1.2.6post2',
     description=('Neutron Scattering software suite.'),
     long_description=long_description,
     author='Jakob Lass',
