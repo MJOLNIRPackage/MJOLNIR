910e5bcf33a5fd324276dea38a45e0bc9619b067
Jakob Lass <lass.jakob@gmail.com>
2024-05-29 09:12:56 +0200

    Add check for minimum colorbar value

--- MJOLNIR/Data/Viewer3D.py
+++ MJOLNIR/Data/Viewer3D.py
@@ -309,7 +309,11 @@ class Viewer3D(object):
             maxVal = np.nanmax(self.masked_array[np.isfinite(self.masked_array)])
         except ValueError:
             maxVal = 1
-        self.caxis = [np.nanmin(self.masked_array[np.isfinite(self.masked_array)]),maxVal]
+        try:
+            minVal = np.nanmin(self.masked_array[np.isfinite(self.masked_array)])
+        except ValueError:
+            minVal = 0
+        self.caxis = [minVal,maxVal]
         if self.grid:
             self.ax.grid(self.grid,zorder=self.gridZOrder)
         else:
