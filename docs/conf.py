0c4d4eb2dde7a8fb0380cbf13fe13393a88d8941
Jakob Lass <lass.jakob@gmail.com>
2024-09-17 14:56:47 +0200

    Update version

--- docs/conf.py
+++ docs/conf.py
@@ -31,9 +31,9 @@ copyright = u'2020, Jakob Lass'
 author = u'Jakob Lass'
 
 # The short X.Y version
-version = u'1.3.2'
+version = u'1.3.2.post3'
 # The full version, including alpha/beta/rc tags
-release = u'1.3.2'
+release = u'1.3.2.post3'
 
 
 
