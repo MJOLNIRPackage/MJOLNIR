b47f7bed0a50b207889ba0cd741e6884738a3e32
Jakob Lass <lass.jakob@gmail.com>
2025-04-17 13:54:44 +0200

    Add AMBERBackground Remove erratic re-calculation of _int

--- MJOLNIR/Data/BackgroundModel.py
+++ MJOLNIR/Data/BackgroundModel.py
@@ -15,6 +15,11 @@ from MJOLNIR.Data import Mask
 import functools
 import warnings
 
+try: 
+    from AMBER import background
+    AMBER_located = True
+except ModuleNotFoundError:
+    AMBER_located = True
 
 
 # If an update has happened to the self.int, and the originalDataSet has a backgroundIntensities pointer array
@@ -241,8 +246,8 @@ class BackgroundObject(object):
         if ax is None:
             fig,ax = plt.subplots()
             
-        if not hasattr(self,'Int'):
-            self.generatePowderBackground()
+        #if not hasattr(self,'_int'):
+        #    self.generatePowderBackground()
             
         
         X,Y = np.meshgrid(self.QBins,self.EnergyBins)
@@ -257,3 +262,159 @@ class BackgroundObject(object):
         ax.set_clim = lambda vmin,vmax: set_clim(ax,vmin,vmax)
         
         return ax
+
+class AMBERBackground(BackgroundObject):
+    def __init__(self, dataset, dQx = 0.03, dQy = 0.03, dE = 0.05, l = None, mu = None, beta = 1.0, backgroundMask=None):
+        """
+        Create an AMBER background model
+
+        Args: 
+            - dataset: MJOLNIR.DataSet
+                DataSet to be utilized for the background
+
+        Kwargs: 
+            - dQx: float
+                Voxel binning along the Qx direction in Ångstrom (default 0.03)
+            - dQy: float
+                Voxel binning along the Qy direction in Ångstrom (default 0.03)
+            - dE: float
+                Voxel binning along the energy direction in meV (default 0.05)
+            - l: float
+                Cost function parameter concerning signal sparcity. If None, found 
+                through the Median Absolute Deviation (MAD) (default None)
+            - mu: float
+                Cost function parameter concerning signal changes along the energy
+                direction. If None, mu is estiamted through the magnitude of the signal 
+                variance along energy (default None)
+            - beta: float
+                Cost function parameter concerning background smoothness. Can be estimated through,
+                a cross validation method. (default 1.0)
+            - backgroundMask: MJOLNIR.Mask
+                Mask to be applied to the data set before AMBER is run (default None)
+        """
+        if not AMBER_located:
+            raise ModuleNotFoundError("The AMBER package was not found. This can be install by\npython -m pypi AMBER-ds4ms")
+        
+        dQ = np.mean([dQx, dQy])
+        super(AMBERBackground,self).__init__(originalDataSet = dataset, dQ=dQ, dE = dE)
+        
+        self.originalDataSet = dataset
+
+        self.dQx = dQx
+        self.dQy = dQy
+
+        self.data, self.bins = self.originalDataSet.binData3D(self.dQx, self.dQy, self.dE)
+
+        # Compute the normalized intensity (I) using binned data
+        self.I = np.divide(self.data[0] * self.data[-1], self.data[1] * self.data[2])
+
+        # Extract coordinates in (Qx, Qy, E) dimensions
+        self.Qx = self.bins[0][:-1, 0, 0]
+        self.Qy = self.bins[1][0, :-1, 0]
+        self.E = self.bins[2][0, 0, :-1]
+
+        self.l = l
+        self.mu = mu
+        self.beta = beta
+
+        self.dtype = self.I.dtype
+
+
+    def set_radial_bins(self, max_radius=None, n_bins=None):
+        """
+        Define ranges for radial bins.
+
+        Args:
+            - max_radius: float
+                Maximum radial distance for the bins. If None found from DataSet (default None)
+            - n_bins: int
+                Number of bins to create. If None found from max_radius and largest of dQx and dQy (default None)
+        """
+        if max_radius is None:
+            local_max = 0
+            for df in self.originalDataSet:
+                local_max = np.max([local_max,np.max(np.linalg.norm([df.qx,df.qy],axis=0))])
+            max_radius = local_max
+        # Set the maximum radial distance and the number of bins
+        self.max_radius = max_radius
+
+        if n_bins is None:
+            dQ = np.max([self.dQx, self.dQy])
+            n_bins = int(np.ceil(self.max_radius/dQ))
+        self.n_bins = n_bins
+
+        # Generate radial bins using torch linspace
+        # The range starts from 0 and goes up to the square root of max_radius, creating n_bins+1 points
+        self.r_range = np.linspace(0, self.max_radius, self.n_bins, dtype=self.dtype)
+        self.dQ = np.mean(np.diff(self.r_range))
+        self.QBins = self.r_range
+        self.EnergyBins = self.E
+
+    def generateAMBER(self, n_epochs=20, verbose=True):
+        """
+        Run AMBER.
+
+        Kwargs: 
+            - n_epocs: int
+                Maximum number of epochs AMBER runs (default 20)
+            - verbose: bool
+                If set True, Display convergence logs (default True)
+        """
+        self.AMBER = background.background(dtype=self.dtype)
+        self.AMBER.set_gridcell_size(dqx=self.dQx, dqy=self.dQy, dE=self.dE)
+        self.AMBER.set_binned_data(Qx=self.Qx, Qy=self.Qy, E=self.E, Int=self.I)
+        self.AMBER.set_radial_bins(max_radius=self.max_radius, n_bins=self.n_bins)
+        self.AMBER.set_variables()
+
+        if self.l is None:
+            self.l = self.AMBER.MAD_lambda()
+
+        if self.mu is None:
+            self.mu = self.AMBER.mu_estimator()
+
+        self.AMBER.denoising(self.AMBER.Ygrid, lambda_=self.l, beta_=self.beta, mu_=self.mu, \
+                             n_epochs=n_epochs, verbose=verbose)
+
+    def applyAMBER(self):
+        self._int = self.AMBER.b.T
+        self.generateFullBackground()
+        
+        self.originalDataSet.backgroundModel = self
+
+    def cross_validation(self, q=0.75, beta=None, l=None, mu=None, n_epochs=15, verbose=True):
+        """
+        Run a cross-validation for a specified set of parameters
+
+        Kwargs: 
+            - q: floay
+                quantile level (default 0.75)
+            beta: list of floats
+                Parameter value beta, if None use previously given beta (default None)
+            l: float
+                Parameter value for lambda, None use previously given beta (default None)
+            mu: float
+                Parameter value for mu, None use previously given beta (default None)
+            - n_epocs: int
+                Maximum number of epochs AMBER runs (default 20)
+            - verbose: bool
+                If set True, Display convergence logs (default True)
+        """
+
+        if not hasattr(self,'AMBER'):
+            self.AMBER = background.background(dtype=self.dtype)
+            self.AMBER.set_gridcell_size(dqx=self.dQx, dqy=self.dQy, dE=self.dE)
+            self.AMBER.set_binned_data(Qx=self.Qx, Qy=self.Qy, E=self.E, Int=self.I)
+            self.AMBER.set_radial_bins(max_radius=self.max_radius, n_bins=self.n_bins)
+            self.AMBER.set_variables()
+
+        if l is None:
+            self.l = self.AMBER.MAD_lambda()
+            l = self.l
+        if mu is None:
+            self.mu = self.AMBER.mu_estimator()
+            mu = self.mu
+        if beta is None:
+            beta = self.beta
+        beta = np.asarray(beta)
+
+        return self.AMBER.cross_validation(q=q, beta_range=beta, lambda_=l, mu_=mu, n_epochs=n_epochs, verbose=verbose)
