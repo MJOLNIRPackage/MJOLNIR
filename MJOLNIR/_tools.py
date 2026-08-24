286c16de83a3b40f9c54bbce432e3744c86268d7
Jakob Lass <jakob.lass@psi.ch>
2026-08-24 15:31:42 +0200

    Add identity function to allow PointerArrays to be pickeled

--- MJOLNIR/_tools.py
+++ MJOLNIR/_tools.py
@@ -1352,6 +1352,10 @@ def histogramdd(sample, bins, weights, returnCounts = False):
     return histograms
 
 
+def identity(x):
+    """Identity function"""
+    return x
+
 class PointerArray():
     """Array-like object designed to facilitate data acquisition from a list of differently sized list of data files having the same attributes.    
 
@@ -1368,7 +1372,7 @@ class PointerArray():
         self._attribute = attribute
         self._datafiles = datafiles
         if function is None:
-            function = lambda x: x
+            function = identity
         self._function = function
         self._shape = None
         self._multiD = None
